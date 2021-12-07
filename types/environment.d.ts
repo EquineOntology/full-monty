a7a7ec6499337c942d20adba216e1e0e7a17c404
Christian Fratta <christian.fratta@gmail.com>
2021-12-07 20:11:02 +0100

    Remove MONGO_DATABASE env

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -1,13 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      GITHUB_AUTH_TOKEN: string;
       NODE_ENV: "development" | "production";
       MONGO_SERVER: string;
       MONGO_PORT: number;
       MONGO_USER: string;
       MONGO_PASSWORD: string;
-      MONGO_DATABASE: string;
     }
   }
 }
