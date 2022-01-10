147735b468c1bb5ebefd3c46bf30cfede419ce92
Christian Fratta <christian.fratta@gmail.com>
2022-01-10 17:01:44 +0100

    Remove generated files

--- api/src/loaders/mongo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const MongoConnector_1 = __importDefault(require("../services/MongoConnector"));
-exports.default = () => {
-    return (0, MongoConnector_1.default)();
-};
