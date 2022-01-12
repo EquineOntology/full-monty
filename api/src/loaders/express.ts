e56153ad6bc5f772b41efb0bbdd943d38bcddfda
Christian Fratta <christian.fratta@gmail.com>
2022-01-12 19:26:26 +0100

    Add cors configuration

--- api/src/loaders/express.ts
+++ api/src/loaders/express.ts
@@ -1,7 +1,9 @@
 import compression from "compression";
+import cors from "cors";
 import express, { Application } from "express";
 
 export default async ({ app }: { app: Application }) => {
+  app.use(cors());
   app.use(compression());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
