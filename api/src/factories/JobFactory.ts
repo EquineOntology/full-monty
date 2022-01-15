e1981fcf062dde42967505f3cf0d64a22881882e
Christian Fratta <christian.fratta@gmail.com>
2022-01-15 21:45:32 +0100

    Allow passing instance attributes to the job factory

--- api/src/factories/JobFactory.ts
+++ api/src/factories/JobFactory.ts
@@ -4,7 +4,13 @@ import TestJob from "../jobs/TestJob";
 
 const classes = { TestJob, MigrateMarvinCsvToMongo };
 
-export default (name: string, parameters?: object): Job => {
+export default (name: string, options?: object, attributes?: object): Job => {
   // See https://stackoverflow.com/a/38127705 for why <any> (CF 19.12.21).
-  return new (<any>classes)[name](parameters);
+  let job = new (<any>classes)[name](options);
+
+  if (attributes) {
+    job = Object.assign(job, attributes);
+  }
+
+  return job;
 };
