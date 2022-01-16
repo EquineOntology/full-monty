1932810fb4056899cd259945d4503a90ef9bcd4f
Christian Fratta <christian.fratta@gmail.com>
2022-01-16 12:10:47 +0100

    Rename main app component

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function FullMonty({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider
       withGlobalStyles
@@ -11,9 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         colorScheme: "light",
       }}
     >
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </MantineProvider>
   );
 }
 
-export default MyApp;
+export default FullMonty;
