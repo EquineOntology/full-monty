70bc47f8288438203cc59100b03de4e54a563158
Christian Fratta <christian.fratta@gmail.com>
2022-01-17 15:58:26 +0100

    Organize imports in import page

--- website/src/pages/import.tsx
+++ website/src/pages/import.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
+import { Container } from "@mantine/core";
 import AppPage from "@/common/components/layout/AppPage";
 import FileUploader from "@/modules/import/FileUploader";
 import ImportList from "@/modules/import/ImportList";
-import { Container, Space } from "@mantine/core";
 
 const Import: NextPage = () => {
   return (
