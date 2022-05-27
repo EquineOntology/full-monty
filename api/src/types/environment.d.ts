95e2ea4a2a0f85dfcc3ff52e64af40a464374a1d
Christian Fratta <christian.fratta@gmail.com>
2022-05-27 17:37:31 +0200

    Remove mongo, add knex & sqlite

--- api/src/types/environment.d.ts
+++ api/src/types/environment.d.ts
@@ -1,10 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      MONGO_PASSWORD: string;
-      MONGO_PORT: number;
-      MONGO_SERVER: string;
-      MONGO_USER: string;
       NODE_ENV: "development" | "production" | "test";
       NODE_PORT: number;
       WEBSITE_URL: string;
