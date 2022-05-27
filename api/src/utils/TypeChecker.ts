a5157f9b8d5a8df7699e86d15178e5f67ab7c800
Christian Fratta <christian.fratta@gmail.com>
2022-05-27 17:39:12 +0200

    Add more type guards to TypeChecker

--- api/src/utils/TypeChecker.ts
+++ api/src/utils/TypeChecker.ts
@@ -10,3 +10,33 @@ export function isNumeric(value: unknown) {
   // isn't hint enough for typescript, so "as" is used (CF 28.02.22).
   return !isNaN(parseFloat(value as string));
 }
+
+export function isObject(value: unknown): value is object {
+  return value !== null && typeof value === "object";
+}
+
+export function isString(value: unknown): value is string {
+  return typeof value === "string";
+}
+
+export function isNumber(value: unknown): value is number {
+  return typeof value === "number";
+}
+
+export function isBool(value: unknown): value is boolean {
+  return typeof value === "boolean";
+}
+
+export function isPrimitive(
+  value: unknown
+): value is string | number | bigint | boolean | symbol | undefined {
+  const valueType = typeof value;
+  return (
+    valueType === "string" ||
+    valueType === "number" ||
+    valueType === "bigint" ||
+    valueType === "boolean" ||
+    valueType === "symbol" ||
+    valueType === "undefined"
+  );
+}
