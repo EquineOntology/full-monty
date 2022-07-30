e9b11b3a3a4157efc6bd448a8d93a27e9e94d9cf
Christian Fratta <christian.fratta@gmail.com>
2022-07-30 12:32:27 +0200

    Add task create/update endpoint

--- api/src/controllers/TaskController.ts
+++ api/src/controllers/TaskController.ts
@@ -1,5 +1,30 @@
 import Task from "@/models/Task";
 
+export async function create(
+  id: string,
+  title: string,
+  done: "Y" | "N",
+  category: string,
+  duration: number,
+  time_estimate: number
+) {
+  try {
+    const task = new Task(
+      id,
+      title,
+      done,
+      category,
+      duration,
+      time_estimate,
+      duration / time_estimate
+    );
+    await task.save();
+    return task;
+  } catch (e) {
+    throw new Error("Could not create task.");
+  }
+}
+
 export async function clear() {
   try {
     await Task.clear();
